7fe6d2a1aceb67affe901812db9ed100b28908cb
JOCECODE <66341350+JOCECODE@users.noreply.github.com>
2020-08-01 00:40:32 -0700

    added row 10 working fine

--- myscript.js
+++ myscript.js
@@ -1,23 +1,42 @@
 var moment = moment();
 var currentDay = $("#currentDay");
-var tA = $("#textArea");
-var saveBtn = $("#saveB");
+var tA9 = $("#textArea9");
+var tA10 = $("#textArea10");
+var saveB9 = $("#saveB9");
+var saveB10 = $("#saveB10");
 
 // CURRENT DAY TO DISPLAY
 currentDay.text(moment.format("dddd, MMMM Do"));
 console.log(moment.format("dddd, MMMM Do"));
 
-// BUTTON CLICK TO DISPLAY TASK SAVED
-saveBtn.on("click", function () {
-  tA.val(function (i, val) {
-    console.log(val);
-    return val;
-  });
+// INIT TIME BLOCKS WITH INFO HOUR 9
+$(function () {
+  var t = localStorage.getItem("9");
+  if (t !== null) {
+    tA9.text("");
+  }
+  tA9.val(t);
 });
-// tA.on("click", function () {
-//     $(this).val(function (i, val) {
-//       console.log(this)
-//       console.log(val);
-//       return val;
-//     });
-//   });
+
+// INIT TIME BLOCKS WITH INFO HOUR 10
+$(function () {
+  var t = localStorage.getItem("10");
+  if (t !== null) {
+    tA10.text("");
+  }
+  tA10.val(t);
+});
+
+// HOUR 9 BLOCK SAVE BUTTON
+saveB9.on("click", function (event) {
+  event.preventDefault();
+  var val = tA9.val();
+  localStorage.setItem("9", val);
+});
+
+// HOUR 10 BLOCK SAVE BUTTON
+saveB10.on("click", function (event) {
+  event.preventDefault();
+  var val = tA10.val();
+  localStorage.setItem("10", val);
+});
