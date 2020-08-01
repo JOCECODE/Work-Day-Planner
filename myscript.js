7f67b460085e1b8ce8c13692750570f59191e580
JOCECODE <66341350+JOCECODE@users.noreply.github.com>
2020-07-31 23:09:59 -0700

    savebtn takes data and shows on block

--- myscript.js
+++ myscript.js
@@ -1,4 +1,23 @@
 var moment = moment();
 var currentDay = $("#currentDay");
+var tA = $("#textArea");
+var saveBtn = $("#saveB");
+
+// CURRENT DAY TO DISPLAY
 currentDay.text(moment.format("dddd, MMMM Do"));
 console.log(moment.format("dddd, MMMM Do"));
+
+// BUTTON CLICK TO DISPLAY TASK SAVED
+saveBtn.on("click", function () {
+  tA.val(function (i, val) {
+    console.log(val);
+    return val;
+  });
+});
+// tA.on("click", function () {
+//     $(this).val(function (i, val) {
+//       console.log(this)
+//       console.log(val);
+//       return val;
+//     });
+//   });
